0fcb9884b0f1c54fc712fe759a52defa3501bbd4
saimkemaloglu <saimkkemal@gmail.com>
2014-02-14 12:53:08 -0800

    MapReduce job
    IMDB movie-actor related job

---
+++ src/main/java/ucsc/hadoop/homework2/Homework2Part1.java
@@ -0,0 +1,12 @@
+package ucsc.hadoop.homework2;
+
+public class Homework2Part1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(Homework2Part1.class.getName());
+	}
+
+}
---
+++ src/main/java/ucsc/hadoop/homework2/Homework2Part2.java
@@ -0,0 +1,12 @@
+package ucsc.hadoop.homework2;
+
+public class Homework2Part2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(Homework2Part2.class.getName());
+	}
+
+}
